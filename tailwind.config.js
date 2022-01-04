dd0a9681f6f9868f2bd32b21a592d9319721a649
ozlemts <ozlemturkerii@gmail.com>
2022-01-05 00:56:24 +0100

    main page design and tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Opensans: ["Open Sans", "cursive"],
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'leaf': {
+          DEFAULT: '#D8EBDF',
+          '50': '#FFFFFF',
+          '100': '#e7fdef',
+          '200': '#b8e1c8',
+          '300': '#b8e1c8',
+          '400': '#b8e1c8',
+          '500': '#b8e1c8',
+          '600': '#b8e1c8',
+          '700': '#5c8e7f',
+          '800': '#294147',
+          '900': '#1d2e31'
+        },
+      },
+    },
   },
   plugins: [],
 }
