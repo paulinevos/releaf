f89e56206d1093da4e3eec01d59763026e61d3f6
ozlemts <ozlemturkerii@gmail.com>
2022-01-20 23:28:53 +0100

    paddigs for team members is decreased

--- components/TeamMember.js
+++ components/TeamMember.js
@@ -1,7 +1,7 @@
 
 export function TeamMember(props) {
   return (
-    <figure className="lg:flex border-leaf-800 border-b p-8">
+    <figure className="lg:flex border-leaf-800 border-b lg:p-8 p-3">
       <img className="w-36 h-36 rounded-full p-4 bg-leaf-400 shadow-inner-lg mx-auto lg:mb-2 mb-6 object-cover" src={props.member.imageurl} alt={props.member.name} width="384" height="512"/>
       <div className="space-y-4 pl-6">
         <blockquote>
