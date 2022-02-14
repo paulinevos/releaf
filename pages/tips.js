3f1362318e7ed8f61c09849f7b01d99272625001
ozlemts <ozlemturkerii@gmail.com>
2022-02-14 17:08:42 +0100

    tips section sample is added

--- pages/tips.js
+++ pages/tips.js
@@ -4,7 +4,30 @@ function Tips() {
   return (
     <div>
       <Layout>
-        <p className="text-lg font-bold">Tips how to reduce your csrbon footprint</p>
+        <div className="ml-8">
+          <div>
+            <p className="font-bold text-sm bg-leaf-800 text-white p-2 rounded my-3 text-center mr-6">TIPS HOW TO REDUCE YOUR CARBON FOOTPRINT</p>
+            <p className="font-bold text-lg">Removing the Plastic</p>
+            <il>
+              <li>Dish soap, even the eco-friendly brands, is always sold in plastic bottles.</li>
+              <li>Dish soap, even the eco-friendly brands, is always sold in plastic bottles.</li>
+              <li>Dish soap, even the eco-friendly brands, is always sold in plastic bottles.</li>
+            </il>
+            <p className="font-bold text-lg mt-4">Save the Water</p>
+            <il>
+              <li>Dish soap, even the eco-friendly brands, is always sold in plastic bottles.</li>
+              <li>Dish soap, even the eco-friendly brands, is always sold in plastic bottles.</li>
+              <li>Dish soap, even the eco-friendly brands, is always sold in plastic bottles.</li>
+            </il>
+
+            <p className="font-bold text-lg mt-4">Reduce Energy Consumption</p>
+            <il>
+              <li>Dish soap, even the eco-friendly brands, is always sold in plastic bottles.</li>
+              <li>Dish soap, even the eco-friendly brands, is always sold in plastic bottles.</li>
+              <li>Dish soap, even the eco-friendly brands, is always sold in plastic bottles.</li>
+            </il>
+          </div>
+        </div>
       </Layout>
     </div>
   );
