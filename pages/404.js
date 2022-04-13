16b6586446e39f6bf96082c9a1aa908a99d78e7c
ozlemts <ozlemturkerii@gmail.com>
2022-04-13 21:35:27 +0200

    deleted 404 page

--- pages/404.js
+++
@@ -1,9 +0,0 @@
-export default function Custom404() {
-  return (
-    <Layout>
-      <div className="flex h-full justify-center items-center">
-        <p className="big-title">404 The Page not Found</p>
-      </div>
-    </Layout>
-    )
-}
