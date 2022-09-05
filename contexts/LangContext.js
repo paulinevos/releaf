1cdfac8d703a02b1c0dcba412d4223a3caadf601
Pauline Vos <pvos88@gmail.com>
2022-09-05 17:08:27 +0200

    Change default language state to Dutch
    As the site will mostly be used for Dutch markets

--- contexts/LangContext.js
+++ contexts/LangContext.js
@@ -3,7 +3,7 @@ import React, { useState, createContext } from "react"
 export const LangContext = createContext(["sw",]);
 
 export const LangProvider = props => {
-  const [lang, setLang] = useState("en")
+  const [lang, setLang] = useState("nl")
   return (
     <LangContext.Provider value={[lang, setLang]}>
       {props.children}
